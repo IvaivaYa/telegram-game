a574a4201745d0e982d868b83941eff71a51ed47
Ivaivaya <ivan19ya@gmail.com>
2024-05-04 12:45:24 +0300

    Новый вариант стартового сообщения (теперь с callback data)

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    start_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    start_gaame_btn = types.KeyboardButton("Да, начать")
+    start_markup = types.InlineKeyboardMarkup()
+    start_gaame_btn = types.InlineKeyboardButton(text = "Да, начать", callback_data="start_game")
     start_markup.add(start_gaame_btn)
 
     global name_of_user
@@ -17,6 +17,11 @@ def start(message):
 
     bot.send_message(message.chat.id, f"Привет, {name_of_user}!\
                     \nТы хочешь начать игру?", reply_markup = start_markup)
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_processing(callback_mess):
+    if callback_mess.data == 'start_game':
+        bot.send_message(callback_mess.message.chat.id, 'Отлично! Тогда начинаем')
 
 @bot.message_handler(content_types=['text'])
 def text(message):
