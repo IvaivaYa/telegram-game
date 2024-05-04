c1861fc7e01d94e7fed89ef7d43abd8b0123ceab
Ivaivaya <ivan19ya@gmail.com>
2024-05-04 12:31:55 +0300

    стартовое сообщение готово

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import telebot 
 from telebot import types
 
-token = 'токен'
+#       https://t.me/for_coding_tg_game_bot
+token = '6313330320:AAHEY7yOlocow6xs1jfwi_ZP8DlXxhLQK14'
 
 bot = telebot.TeleBot(token)
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, message.text)
+    start_markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    start_gaame_btn = types.KeyboardButton("Да, начать")
+    start_markup.add(start_gaame_btn)
+
+    global name_of_user
+    name_of_user = message.from_user.first_name
+
+    bot.send_message(message.chat.id, f"Привет, {name_of_user}!\
+                    \nТы хочешь начать игру?", reply_markup = start_markup)
 
 @bot.message_handler(content_types=['text'])
 def text(message):
-    bot.send_message(message.chat.id, message.text)
-
+    bot.send_message(message.chat.id, f"{name_of_user} написал: {message.text}")
 
 bot.infinity_polling()
