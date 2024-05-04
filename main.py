d71b7d2030d8a12f8ecc4e579f85aa86eadc9abb
Ivaivaya <ivan19ya@gmail.com>
2024-05-04 12:10:00 +0300

    начало работы

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-# sdf
+import telebot 
+from telebot import types
+
+token = 'токен'
+
+bot = telebot.TeleBot(token)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, message.text)
+
+@bot.message_handler(content_types=['text'])
+def text(message):
+    bot.send_message(message.chat.id, message.text)
+
+
+bot.infinity_polling()
